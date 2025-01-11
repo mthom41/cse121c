74bb726460f6aa6dfafc47db646d63cde0814fcd
mthom41 <mckayashe@gmail.com>
2025-01-11 16:59:02 -0700

    wk 1 hw start

---
+++ hey/src/hey.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hey.c
+ Author      : mckay t
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ variables/src/variables.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : variables.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i=0;
+	printf("Enter a number: ");
+	scanf("%d", &i);
+}
